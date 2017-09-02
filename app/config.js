b99c95f5ce354091ff4f612bb249911095e8a862
bspview <zanewom@p33.org>
2017-09-02 18:00:06 +0500

    Add prg

---
+++ app/config.js
@@ -0,0 +1,21 @@
+var GB_CONFIG = {
+	
+	entryPointUrl:   'index.html',
+
+	configUrl:		 'app/config.js',
+
+	appUrl:          'app/app.js',
+
+	baseMapsUrl:     'data/_maps/',
+
+	baseReourcesUrl: 'data/_resources/',
+	
+	baseResourceExt: '.bin',
+	
+	resourceExt:	 '.bin',
+	
+	appUpdateId:      8,
+	
+	global:           {},
+
+};
